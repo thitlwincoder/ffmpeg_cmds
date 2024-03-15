d7de3e90500c9fc8c7969224269eef525c43de27
thitlwincoder <thitlwincoder@gmail.com>
2024-03-15 21:53:41 +0630

    feat: add `filter`

---
+++ lib/src/filter.dart
@@ -0,0 +1,23 @@
+/// CMD
+class Filter {
+  /// CMD
+  Filter({
+    required this.name,
+    required this.options,
+  });
+
+  /// key
+  final String name;
+
+  /// options
+  final Map<String, dynamic> options;
+
+  @override
+  String toString() {
+    options.removeWhere((key, value) => value == null);
+
+    final cmds = options.keys.map((e) => '$e=${options[e]}').join(':');
+
+    return '$name=$cmds';
+  }
+}
