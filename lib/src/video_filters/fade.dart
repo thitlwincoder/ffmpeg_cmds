781b91bf0dcf83d643cd82fe8dec5f8170224d43
thitlwincoder <thitlwincoder@gmail.com>
2024-03-18 09:56:38 +0630

    feat: add `fade`

---
+++ lib/src/video_filters/fade.dart
@@ -0,0 +1,62 @@
+import 'package:ffmpeg_cmds/ffmpeg_cmds.dart';
+
+/// Apply a fade-in/out effect to the input video.
+class Fade implements Filter {
+  /// Fade
+  Fade({
+    this.type,
+    this.t,
+    this.startFrame,
+    this.s,
+    this.nbFrames,
+    this.n,
+    this.alpha,
+    this.startTime,
+    this.st,
+    this.duration,
+    this.d,
+    this.color,
+    this.c,
+  });
+
+  /// The effect type can be either "in" for a fade-in, or "out" for a fade-out effect. Default is `in`.
+  String? type, t;
+
+  /// Specify the number of the frame to start applying the fade effect at. Default is `0`.
+  int? startFrame, s;
+
+  /// The number of frames that the fade effect lasts. At the end of the fade-in effect, the output video will have the same intensity as the input video. At the end of the fade-out transition, the output video will be filled with the selected color. Default is `25`.
+  int? nbFrames, n;
+
+  /// If set to 1, fade only alpha channel, if one exists on the input. Default value is `0`.
+  int? alpha;
+
+  /// Specify the timestamp (in seconds) of the frame to start to apply the fade effect. If both [startFrame] and [startTime] are specified, the fade will start at whichever comes last. Default is `0`.
+  int? startTime, st;
+
+  /// The number of seconds for which the fade effect has to last. At the end of the fade-in effect the output video will have the same intensity as the input video, at the end of the fade-out transition the output video will be filled with the selected color. If both [duration] and [nbFrames] are specified, duration is used. Default is `0` ([nbFrames] is used by default).
+  int? duration, d;
+
+  /// Specify the color of the fade. Default is `black`.
+  String? color, c;
+
+  @override
+  String get name => 'fade';
+
+  @override
+  Map<String, dynamic> get options => {
+        'type': type,
+        't': t,
+        'start_frame': startFrame,
+        's': s,
+        'nb_frames': nbFrames,
+        'n': n,
+        'alpha': alpha,
+        'start_time': startTime,
+        'st': st,
+        'duration': duration,
+        'd': d,
+        'color': color,
+        'c': c,
+      };
+}
